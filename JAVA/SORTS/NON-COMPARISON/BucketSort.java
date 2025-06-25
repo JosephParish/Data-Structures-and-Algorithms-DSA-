08a114d37cab78de206813899756983e158f2d2b
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-06-25 23:51:10 +0100

    Update BucketSort.java

--- JAVA/SORTS/NON-COMPARISON/BucketSort.java
+++ JAVA/SORTS/NON-COMPARISON/BucketSort.java
@@ -2,9 +2,11 @@
  * <p> BucketSort Algorithm to sort unsorted arrays</p>
  *
  * @author Joseph Parish.
- * @version 0.0.0
+ * @version 0.0.3
  * Last Changed: 25/06/25
  */
 public class BucketSort {
-  
+  satic void bucketSort(){
+    
+  }
 }
