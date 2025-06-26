ca5926a0b54699843e4652bb5367d847a2e7fef8
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-06-26 23:18:20 +0100

    Update BucketSort.java

--- JAVA/SORTS/NON-COMPARISON/BucketSort.java
+++ JAVA/SORTS/NON-COMPARISON/BucketSort.java
@@ -2,14 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <p> BucketSort Algorithm to sort unsorted arrays</p>
+ * <p> BucketSort Algorithm to sort unsorted arrays (of decimals),
+ * using separte lists (buckets) and another sorting algorithm (insertion sort)</p>
  *
  * @author Joseph Parish.
- * @version 0.0.3
- * Last Changed: 25/06/25
+ * @version 1.0.6
+ * Last Changed: 26/06/25
  */
 public class BucketSort {
-  
+
+  /**
+    * BucketSort main body - sorts the elements into buckets (lists) via first significant bit
+    * then performing a sorting algorithm (insertion sort) and then recombining the buckets.
+    * 
+    * @param array - array to be sorted
+    */
   satic void bucketSort(float[] array) {
       int n = array.length;
       List<Float>[] buckets = new ArrayList[n];
@@ -32,7 +39,13 @@ public class BucketSort {
           }
       }
   }
-  
+
+  /**
+     * InsertionSort, loops though elements
+     * placing current element behind all larger elements behind it
+     *
+     * @param bucket - list to be sorted
+     */
   public static void insertionSort(List<Float> bucket) {
       for (int i = 1; i < bucket.size(); ++i) {
           float key = bucket.get(i);
