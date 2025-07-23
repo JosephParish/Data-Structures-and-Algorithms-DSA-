8191889a6923ed3f953e2b5fb57970dd1ee0f97e
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-07-23 22:30:19 +0100

    Update DFS.java

--- JAVA/SEARCHES/DFS.java
+++ JAVA/SEARCHES/DFS.java
@@ -2,21 +2,35 @@ import java.util.ArrayList;
 
 public class DFS<T> {
 
-    public static <T> ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj, int source, T target) {
-        boolean[] visited = new boolean[adj.size()];
-        ArrayList<Integer> traversal = new ArrayList<>();
-        dfsRec(adj, visited, source, traversal, target);
+    public static <T> ArrayList<T> dfs(ArrayList<ArrayList<T>> adj, T source, T target) {
+        
+        ArrayList<Boolean> visited = new ArrayList<>(adj.size());
+        visited.fill(false);
+        ArrayList<T> traversal = new ArrayList<>();
+        
+        dfsUtil(adj, visited, source, traversal, target);
         return traversal;
     }
-  
-    private static <T> void dfsUtil(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int currentNode, ArrayList<Integer> traversal, T target) {
-        visited[currentNode] = true;
+
+    private static <T> boolean dfsUtil(ArrayList<ArrayList<T>> adj, ArrayList<Boolean> visited, T currentNode, ArrayList<T> traversal, T target) {
+        
+        int currentIndex = adj.indexOf(currentNode);
+        if (currentIndex == -1) return false;
+        
+        visited.set(currentIndex, true);
         traversal.add(currentNode);
-      
-        for (int i : adj.get(currentNode)) {
-            if (!visited[i] && currentNode != target) {
-                dfsRec(adj, visited, i, traversal, target);
+        
+        if (currentNode.equals(target)) {
+            return true;
+        }
+
+        for (T neighbor : adj.get(currentIndex)) {
+            int neighborIndex = adj.indexOf(neighbor);
+            if (!visited.get(neighborIndex)) {
+                dfsUtil(adj, visited, neighbor, traversal, target);
             }
         }
+
+        return false;
     }
 }
