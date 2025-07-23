c5582be1f05e2de97d4726a491807ba134a43f70
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-07-23 22:18:18 +0100

    Update DFS.java

--- JAVA/SEARCHES/DFS.java
+++ JAVA/SEARCHES/DFS.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+public class DFS<T> {
+
+    public static <T> ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj, int source, T target) {
+        boolean[] visited = new boolean[adj.size()];
+        ArrayList<Integer> traversal = new ArrayList<>();
+        dfsRec(adj, visited, source, traversal, target);
+        return traversal;
+    }
+  
+    private static <T> void dfsUtil(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int currentNode, ArrayList<Integer> traversal, T target) {
+        visited[currentNode] = true;
+        traversal.add(currentNode);
+      
+        for (int i : adj.get(currentNode)) {
+            if (!visited[i] && currentNode != target) {
+                dfsRec(adj, visited, i, traversal, target);
+            }
+        }
+    }
+}
