0e13dd6819a1435feb2cc0065e47ff512bfb2689
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-09-03 13:30:53 +0100

    Update HEAP.java

--- JAVA/DATA STRUCTURES/HEAP.java
+++ JAVA/DATA STRUCTURES/HEAP.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class HEAP {
   private TREE binaryTree;
-  private boolean isMinHeap;
+  private boolean isMin;
 
 //comparitor
   
-  public HEAP(TREE binaryTree, boolean isMinHeap){ // and pass a comparitor function for T
+  public HEAP(TREE binaryTree, boolean isMin){ // and pass a comparitor function for T
     this.binaryTree = binaryTree;
-    this.isMinHeap = isMinHeap;
+    this.isMinHeap = isMin;
 
     //comparitor
   }
@@ -17,21 +17,21 @@ public class HEAP {
     // use comparitor on binaryTree
   }
 
-  public void addNode(NODE node){
+  public void add(NODE node){ //perhaps change node to be a value that is then declared as a node and possibly check if it is the same type as the rest of the nodes (integer, string etc)
     binaryTree.addNode(node);
     sortHeap();
   }
-  public void removeNode(){
+  public void remove(NODE node){
     binaryTree.removeNode(node);
     sortHeap();
   }
-  public NODE getRootNode(){
+  public NODE getRoot(){
     return binaryTree.getRootNode();
   }
-  public boolean getIsMinHeap(){
-    return isMinHeap;
+  public boolean getIsMin(){
+    return isMin;
   }
-  public TREE getBinaryTree(){
+  public TREE getHeap(){
     return binaryTree;
   }
 }
